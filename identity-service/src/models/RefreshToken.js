4be5faa72801cb2c5d2f02079dc1a78bbea6744f
Deepansh <dee.pansh18johri@gmail.com>
2025-02-16 12:20:23 +0530

    first commit

---
+++ identity-service/src/models/RefreshToken.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const refreshTokenSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    expiresAt: {
+        type: Date,
+        required: true
+    },
+},
+    {
+        timestamps: true
+    }
+);
+
+
+/*
+Time to live index (TTL index) in MongoDB : autmat. deletes expired documents after specified amount of time 
+
+Under the hood : we create TTL index on Date fleid, then mongoDB monitors this field and deletes docs once the time is reached.
+
+expireAfterSeconds	                      Deletion                                       
+     0	                       Deletes exactly when expiresAt is reached.
+     1	                       Deletes 1 second after expiresAt.
+    3600	                   Deletes 1 hour after expiresAt.
+
+*/
+refreshTokenSchema.index({ expiresAt: 1, expireAfterSeconds: 0 });
+
+const RefreshToken = mongoose.model("RefreshToken",refreshTokenSchema);
+
+module.exports = RefreshToken;
---
+++ identity-service/src/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const argon2 = require("argon2");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true
+});
+
+userSchema.pre("save", async function hashPassword(next) {
+    if (this.isModified("password")) {
+        try {
+            this.password = await argon2.hash(this.password);
+            next();
+        }
+        catch (error) {
+            return next(error);
+        }
+    }
+});
+
+userSchema.methods.comparePassword = async function (inputPassword) {
+    try {
+        return await argon2.verify(this.password, inputPassword);
+    } catch (error) {
+        throw error;
+    }
+}
+
+userSchema.index({ username: "text" });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
